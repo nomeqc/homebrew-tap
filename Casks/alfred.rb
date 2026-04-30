bf0becd416231aaa3419cc1320c7ff6069c4f906
nomeqc <nomeqc@gmail.com>
2026-04-30 10:04:29 +0530

    update

--- Casks/alfred.rb
+++ Casks/alfred.rb
@@ -6,7 +6,7 @@ cask "alfred" do
   url "https://github.com/nomeqc/my-release/releases/download/20260429/Alfred_5.7.2.dmg"
   
   name "Alfred"
-  desc ""
+  desc " "
   
   # 这里的名字必须和 DMG 挂载后看到的 .app 名字一模一样
   app "Alfred.app"
