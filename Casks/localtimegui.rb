9d9bf41cc2a0a17d28363d040936246b298bcef3
nomeqc <nomeqc@gmail.com>
2026-05-15 13:15:14 +0530

    update

--- Casks/localtimegui.rb
+++ Casks/localtimegui.rb
@@ -1,13 +1,13 @@
-cask "alfred" do
-  version "5.7.2"
+cask "localtimegui" do
+  version "1.0.0"
   sha256 :no_check # 跳过哈希校验，防止以后更新 DMG 时报错
 
   # 替换为你实际的 DMG 下载链接
-  url "https://github.com/nomeqc/my-release/releases/download/20260429/Alfred_5.7.2.dmg"
+  url "https://github.com/nomeqc/my-release/releases/download/20260429/LocalTimeGUI.dmg"
   
-  name "Alfred"
+  name "LocalTimeGUI"
   desc ""
   
   # 这里的名字必须和 DMG 挂载后看到的 .app 名字一模一样
-  app "Alfred.app"
+  app "LocalTimeGUI.app"
 end
