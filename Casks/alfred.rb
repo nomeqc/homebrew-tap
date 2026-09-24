e06d9d15c6ca3a10d1de62b366935cd3ca3324ef
nomeqc <nomeqc@gmail.com>
2026-09-24 14:20:50 +0530

    update

--- Casks/alfred.rb
+++ Casks/alfred.rb
@@ -1,9 +1,9 @@
 cask "alfred" do
-  version "5.8"
+  version "5.7.2"
   sha256 :no_check # 跳过哈希校验，防止以后更新 DMG 时报错
 
   # 替换为你实际的 DMG 下载链接
-  url "https://github.com/nomeqc/my-release/releases/download/20260429/Alfred_5.8.dmg"
+  url "https://github.com/nomeqc/my-release/releases/download/20260429/Alfred_5.7.2.dmg"
   
   name "Alfred"
   desc ""
--- Casks/istatmenus.rb
+++ Casks/istatmenus.rb
@@ -1,9 +1,9 @@
 cask "istatmenus" do
-  version "7.20.7"
+  version "7.50.1"
   sha256 :no_check # 跳过哈希校验，防止以后更新 DMG 时报错
 
   # 替换为你实际的 DMG 下载链接
-  url "https://github.com/nomeqc/my-release/releases/download/20260429/iStat_Menus_7.20.7.dmg"
+  url "https://github.com/nomeqc/my-release/releases/download/20260429/iStat_Menus_7.50.1.dmg"
   
   name "iStat Menus"
   desc ""
